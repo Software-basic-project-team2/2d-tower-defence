efaa48f4bcf874e65ca9216f4108e7c2e57034e3
HongSik Cho <daolove0323@gmail.com>
2023-05-24 01:27:29 +0900

    hongsik

---
+++ Assets/script_hongsik.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_jangan : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //이장안 연동완료!!
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
