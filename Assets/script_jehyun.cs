eeeed8c5bfbcdc5c23350076492f2be9f5e06ac3
ArkKorea <cjjj1102@naver.com>
2023-05-24 00:40:22 +0800

    제현연동
    된건가?

---
+++ Assets/script_jehyun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_jehyun : MonoBehaviour
+{
+    //오... 된건가?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
