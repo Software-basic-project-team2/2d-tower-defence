38d14c7045d4bfd90afa279c426f2f414ba7f745
테크 <lja3723@gmail.com>
2023-05-25 03:15:18 +0900

    TowerBuilder 추가 / TowerController 변경

---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tower
+{ 
+    public Vector2 Position { get; set; }
+    public float AttackRadius { get; set; }
+    public float AttackCycleSecond { get; set; }
+    public int Damage { get; set; }
+
+    public static TowerBuilder Builder(TowerType towerType)
+    {
+        return new TowerBuilder(towerType);
+    }
+
+    public void Attack(Enemy enemy)
+    {
+        enemy.Hp -= Damage;
+        Debug.Log("Current HP: " + enemy.Hp);
+    }
+}
+
+#region Derived Tower 정의
+public class Tower1 : Tower
+{
+
+}
+
+public class Tower2 : Tower
+{
+
+}
+
+public class Tower3 : Tower
+{
+
+}
+
+//새 타입의 타워가 추가되면 이곳을 수정해야 함
+#region Tower 타입 및 TowerBuilder 정의
+public enum TowerType
+{
+    Tower1, Tower2, Tower3
+};
+
+//디자인패턴: 빌더 패턴을 따름    
+public class TowerBuilder
+{
+    private Tower tower;
+
+    public TowerBuilder(TowerType type)
+    {
+        switch (type)
+        {
+            case TowerType.Tower1:
+                tower = new Tower1(); break;
+            case TowerType.Tower2:
+                tower = new Tower2(); break;
+            case TowerType.Tower3:
+                tower = new Tower3(); break;
+        }
+    }
+
+    public TowerBuilder Position(Vector2 position)
+    {
+        tower.Position = position; return this;
+    }
+
+    public TowerBuilder AttackRadius(float attackRadius)
+    {
+        tower.AttackRadius = attackRadius; return this;
+    }
+
+    public TowerBuilder AttackCycleSecond(float attackCycleSecond)
+    {
+        tower.AttackCycleSecond = attackCycleSecond; return this;
+    }
+
+    public TowerBuilder Damage(int damage)
+    {
+        tower.Damage = damage; return this;
+    }
+
+    public Tower Build()
+    {
+        return tower;
+    }
+}
+#endregion
+
+#endregion
---
+++ Assets/Scripts/Tower/TowerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Function: 타워를 컨트롤한다.
+//  
+public class TowerController : MonoBehaviour
+{
+    public TowerType TowerType; //컨트롤할 타워 유형을 외부에서 받아옴
+
+    private Tower tower;
+    private bool attackable;
+    private Enemy attackingEnemy;
+
+    private void Awake()
+    {
+        CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+        tower = Tower.Builder(TowerType)
+            .Position(transform.position)
+            .AttackRadius(circleCollider.radius)
+            .AttackCycleSecond(1)
+            .Damage(5)
+            .Build();
+        attackable = true;
+        attackingEnemy = null;
+    }
+
+    public void Attack()
+    {
+        if (!attackable) return;
+        if (attackingEnemy == null) return;
+
+        tower.Attack(attackingEnemy);
+
+        StartCoroutine("SetAttackCycle");
+    }
+
+    IEnumerator SetAttackCycle()
+    {
+        attackable = false;
+        yield return new WaitForSeconds(tower.AttackCycleSecond);
+        attackable = true;
+    }
+
+    private void Update()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(tower.Position, tower.AttackRadius);
+
+        if (colliders.Length != 0)
+        {
+            attackingEnemy = colliders[0].gameObject.GetComponent<Enemy>();
+            Attack();
+        }
+        else
+        {
+            attackingEnemy = null;
+        }
+    }
+}
