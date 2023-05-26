dad49bc18c3d37bbea876fc97abf041ab45f7c44
dksltq <jyepjhh@naver.com>
2023-05-26 20:46:17 +0900

    waypoint
    데모

---
+++ Assets/WayPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public Transform[] waypoints; // Waypoints를 저장할 배열
+    public float moveSpeed = 5f; // 캐릭터의 이동 속도
+    private int currentWaypointIndex = 0; // 현재 Waypoint 인덱스
+
+    private void Update()
+    {
+        // 현재 Waypoint를 향해 이동
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, moveSpeed * Time.deltaTime);
+
+        // 현재 Waypoint에 도착한 경우 다음 Waypoint로 이동
+        if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
+        {
+            currentWaypointIndex++;
+
+
+        }
+    }
+}
