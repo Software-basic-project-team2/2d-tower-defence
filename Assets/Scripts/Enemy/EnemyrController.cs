9330a6e3ae1d5ffeca1b9c0cf167652e2e01ac81
dksltq <jyepjhh@naver.com>
2023-05-31 15:23:19 +0900

    메인씬 + 이동 코드
    메인씬 생성하고 이동 코드 수정

--- Assets/Scripts/Enemy/EnemyrController.cs
+++ Assets/Scripts/Enemy/EnemyrController.cs
@@ -1,15 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 
-public class WayPoint : MonoBehaviour
+public class EnemyrController : MonoBehaviour
 {
     public Transform[] waypoints; // Waypoints를 저장할 배열
     public float moveSpeed = 5f; // 캐릭터의 이동 속도
     private int currentWaypointIndex = 0; // 현재 Waypoint 인덱스
 
+    private void Start()
+    {
+        if (GameManager.instance.easyMode) // 이지 모드가 선택되었을 때
+        {
+            waypoints = GameObject.FindGameObjectsWithTag("easy").Select(obj => obj.transform).ToArray();
+        }
+
+        if (GameManager.instance.hardMode) // 하드 모드가 선택되었을 때
+        {
+            waypoints = GameObject.FindGameObjectsWithTag("hard").Select(obj => obj.transform).ToArray();
+        }
+
+
+    }
+
     private void Update()
     {
+        if (waypoints == null || waypoints.Length == 0)
+            return;
+
         // 현재 Waypoint를 향해 이동
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, moveSpeed * Time.deltaTime);
 
@@ -17,8 +34,6 @@ public class WayPoint : MonoBehaviour
         if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
         {
             currentWaypointIndex++;
-
-
         }
     }
 }
