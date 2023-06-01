051c78b51e1c592f7190834ac52d72d2f57ec306
lja3723 <lja3723@gmail.com>
2023-06-01 22:01:05 +0900

    Tower Builder 개발

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 
 public abstract class Tower : MonoBehaviour
 {
-    //타워 상태 저장변수
-    public const int MaxLevel = 3;
-    public float AttackRadius { get; set; }
-    public float AttackCycleSecond { get; set; }
-    public int Damage { get; set; }
-    public int Level { get; set; }
+    public static readonly int MaxLevel = 3;
+    public static readonly int TypeCount = 4;
 
+    #region Outer Reference
     //레벨마다 다른 모양이 되기위한 참조변수
     private SpriteRenderer body;
     private SpriteRenderer plate_front;
     private SpriteRenderer plate_back;
-    private bool attackable;
+    #endregion
 
+    //타워 상태 저장변수
+    #region Tower Variables
+    public float AttackRadius //공격 범위
+    {
+        get {
+            return GetComponent<CircleCollider2D>().radius;
+        }
+        set {            
+            GetComponent<CircleCollider2D>().radius = value;
+        }
+    } 
+    public float AttackCycleSecond { get; set; } //공격 주기
+    public int Damage { get; set; } //타워 공격력
+    public int Level 
+    {
+        get { return level; }
+        set
+        {
+            if (!(1 <= value && value <= 3)) value = 1;
+            level = value;
+        } 
+    } private int level; //타워 레벨
+
+    private bool attackable;
+    #endregion
+
+    #region Initialize Logic
     private void Awake()
     {
         //다른 모양이 되기위해 필요한 참조 연결
         Transform tBody = transform.Find("body");
         Transform tPlate = tBody.Find("plate");
+
         body = tBody.GetComponent<SpriteRenderer>();
         plate_front = tPlate.Find("front").GetComponent<SpriteRenderer>();
         plate_back = tPlate.Find("back").GetComponent<SpriteRenderer>();
@@ -41,19 +66,34 @@ public abstract class Tower : MonoBehaviour
         //타워 상태 설정
 
         Level = 3;
-        AttackRadius = GetComponent<CircleCollider2D>().radius;
         AttackCycleSecond = 1f;
         Damage = 5;
         attackable = true;
-        MatchSprite();
+        LoadSprites();
+        SyncSprite();
     }
 
-    /* (무시(지우지는 X))
-    public static TowerBuilder Builder(TowerType towerType)
+    //타워 스프라이트 이미지 통합 로드(최초 1회 실행)
+    private static Sprite[] TowerSprites;
+    private static bool isSpriteLoad = false;
+    private static void LoadSprites()
     {
-        return new TowerBuilder(towerType);
-    }*/
+        if (isSpriteLoad) return;
+        TowerSprites = Resources.LoadAll<Sprite>("TowerImages");
+        isSpriteLoad = true;
+    }
+    #endregion
 
+    #region Tower Builder Logic
+    public enum Type { Tower1, Tower2, Tower3, Tower4 }
+
+    public static TowerBuilder Builder()
+    {
+        return TowerBuilder.Instance();
+    }
+    #endregion
+
+    #region Update Logic
     private void Update()
     {
         Enemy target = FindAttackable(); //공격 대상 탐색
@@ -92,19 +132,11 @@ public abstract class Tower : MonoBehaviour
         yield return new WaitForSeconds(AttackCycleSecond);
         attackable = true;
     }
+    #endregion
 
-    private static Sprite[] TowerSprites;
-    private static bool isSpriteLoad = false;
-
-    protected virtual void MatchSprite()
+    public virtual void SyncSprite()
     {
         const int Body = 0, PlateFront = 1, PlateBack = 2;
-
-        //타워 스프라이트 이미지 통합 로드(최초 1회 실행)
-        if (!isSpriteLoad) { 
-            TowerSprites = Resources.LoadAll<Sprite>("TowerImages");
-            isSpriteLoad = true;
-        }
 
         //현재 레벨에 맞는 스프라이드를 동적으로 참조
         body.sprite = TowerSprites[SpriteIndex(Level, Body) - 1];
--- Assets/Scripts/Tower/TowerBuilder.cs
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -1,64 +1,76 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//이 부분은 일단 무시
 //디자인패턴: 빌더 패턴을 따름    
-public class TowerBuilder : MonoBehaviour
+public class TowerBuilder
 {
-    private Tower tower;
+    private static GameObject[] towers;
+    //instantiate로 복제할 타워 원본 프리팹 생성 (최초 1회)
+    private static void LoadTowers()
+    {
+        if (towers != null) return;
+        towers = new GameObject[Tower.TypeCount];
+        for (int i = 0; i < Tower.TypeCount; i++)
+        {
+            towers[i] = Resources.Load<GameObject>("Prefabs\\Tower\\Tower" + (i + 1));
+            Debug.Log(towers[i].name);
+        }
+    }
 
-    //public TowerBuilder(TowerType type)
-    //{
-    //    switch (type)
-    //    {
-    //        case TowerType.Tower1:
-    //            tower = new Tower1(); break;
-    //        case TowerType.Tower2:
-    //            tower = new Tower2(); break;
-    //        case TowerType.Tower3:
-    //            tower = new Tower3(); break;
-    //        case TowerType.Tower4:
-    //            tower = new Tower4(); break;
-    //    }
-    //}
+    #region Sington
+    private static TowerBuilder singleton = new TowerBuilder();
+    private TowerBuilder() {
+        if (towers == null) LoadTowers();
+    }
+    public static TowerBuilder Instance() { return singleton; }
+    
 
-    //public TowerBuilder GameObjectReference(GameObject ref)
-    //{
-    //    tower.gameObject = ref;
-    //}
+    #endregion
 
-    //public TowerBuilder ConnectSprite(SpriteRenderer body, SpriteRenderer front, SpriteRenderer back)
-    //{
-    //    tower.body = body;
-    //    tower.plate_front = front;
-    //    tower.plate_back = back;
-    //    return this;
-    //}
+    private Vector2 position;
+    Tower.Type type;
+    private int level;
+    private float attackRadius;
+    private float attackCycleSecond;
+    private int damage;
 
+    public TowerBuilder Position(Vector2 position)
+    {
+        this.position = position; return this;
+    }
+    public TowerBuilder Type(Tower.Type type) 
+    {
+        this.type = type; return this; 
+    }
     public TowerBuilder Level(int level)
     {
-        tower.Level = level; return this;
+        this.level = level; return this;
     }
-
     public TowerBuilder AttackRadius(float attackRadius)
     {
-        tower.AttackRadius = attackRadius; return this;
+        this.attackRadius = attackRadius; return this;
     }
-
     public TowerBuilder AttackCycleSecond(float attackCycleSecond)
     {
-        tower.AttackCycleSecond = attackCycleSecond; return this;
+        this.attackCycleSecond = attackCycleSecond; return this;
     }
-
     public TowerBuilder Damage(int damage)
     {
-        tower.Damage = damage; return this;
+        this.damage = damage; return this;
     }
+    public GameObject Build()
+    {
+        GameObject obj = GameObject.Instantiate(towers[(int)type], position, Quaternion.identity, GameObject.Find("Towers").transform);
+        Tower tower = obj.GetComponent<Tower>();
 
-    //public Tower Build()
-    //{
-    //    tower.MatchSprite();
-    //    return tower;
-    //}
+        tower.Level = level;
+        tower.AttackRadius = attackRadius;
+        tower.AttackCycleSecond = attackCycleSecond;
+        tower.Damage = damage;
+        tower.SyncSprite();
+
+        return obj;
+    }
 }
