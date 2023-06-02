46f1abe411815eda9202f64c9ceb375f05f13071
lja3723 <lja3723@gmail.com>
2023-06-03 02:59:12 +0900

    Background Loader 추가 / Game/StartManager 개선

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameMode { Easy, Hard }
+
+public class GameManager : MonoBehaviour
+{
+    public GameMode InitialGameMode; //기본 게임모드 (inspector에서 설정)
+    public GameMode Mode { get; private set; }
+
+    public void LoadGameScene(GameMode mode)
+    {
+        StartCoroutine("LoadGameSceneAsync", mode);
+    }
+
+    IEnumerator LoadGameSceneAsync(GameMode mode)
+    {
+        // 게임 씬으로 전환
+        SceneManager.LoadScene("GameScene");
+        Mode = mode;
+        yield return new WaitWhile(() => GameObject.Find("Backgrounds") == null);
+        BackgroundLoader.Load();
+    }
+    
+    
+    private void Awake()
+    {
+        Singleton();
+        Mode = InitialGameMode;
+    }
+
+    #region 싱글톤 코드
+    public static GameManager instance; // 싱글톤 인스턴스
+    private void Singleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion   
+}
+
---
+++ Assets/Scripts/Manager/StartManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//MainScene의 이벤트 클래스
+public class StartManager : MonoBehaviour 
+{
+    //Easy Button Event
+    public void StartEasyMode()
+    {
+        // 이지 모드를 선택하여 GameManager에 반영
+        GameManager.instance.LoadGameScene(GameMode.Easy);
+    }
+
+    //Hard Button Event
+    public void StartHardMode()
+    {
+        // 하드 모드를 선택하여 GameManager에 반영
+        GameManager.instance.LoadGameScene(GameMode.Hard);
+    }
+
+    //Exit Button Event
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
