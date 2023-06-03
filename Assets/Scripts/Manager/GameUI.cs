c9deb03542e56ba3a73e6a84c9f7fe647c817f6c
dksltq <jyepjhh@naver.com>
2023-06-04 01:10:47 +0900

    ui, 프리팹 수정
    ui, 프리팹 수정

---
+++ Assets/Scripts/Manager/GameUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject gameUI;
+    private bool isPaused = false;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        gameUI.SetActive(false);
+        isPaused = false;
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        gameUI.SetActive(true);
+        isPaused = true;
+    }
+
+    public void OnMainButtonClicked()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    //리셋버튼
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
