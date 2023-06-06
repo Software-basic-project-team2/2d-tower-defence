cbf272e8c8aa387a6c326041980831e64fe30608
ArkKorea <cjjj1102@naver.com>
2023-06-07 00:19:09 +0800

    복제 스크립트

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab; // 적 프리팹
+    [SerializeField]
+    private float spawnTime; // 적 생성주기
+    [SerializeField]
+    private Transform[] wayPoints;
+
+    private void Awake()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            GameObject clone = Instantiate(enemyPrefab);
+            Enemy enemy = clone.GetComponent<Enemy>();
+
+            enemy.Setup(wayPoints);
+
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
