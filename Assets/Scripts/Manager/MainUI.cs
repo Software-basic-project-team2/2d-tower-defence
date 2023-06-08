bcea43adadf41c59aa18bdd2bbdb9e253faee3b6
HongSik Cho <daolove0323@gmail.com>
2023-06-09 03:09:45 +0900

    main수정

--- Assets/Scripts/Manager/MainUI.cs
+++ Assets/Scripts/Manager/MainUI.cs
@@ -6,22 +6,67 @@ using UnityEngine.SceneManagement;
 //MainScene의 이벤트 클래스
 public class MainUI : MonoBehaviour 
 {
+<<<<<<< Updated upstream
     public void OnEasyButtonClicked()
+=======
+    public TMP_Text BeginnerText;
+    public TMP_Text IntermediateText;
+    public TMP_Text AdvancedText;
+    public TMP_Text ExpertText;
+    public GameObject triangle1;
+    public GameObject triangle2;
+    public GameObject triangle3;
+    public GameObject triangle4;
+
+    private void Start()
     {
-        // 이지 모드를 선택하여 GameManager에 반영
+    }
+
+    public void OnButtonPointerExit()
+    {
+        BeginnerText.color = Color.white;
+        IntermediateText.color = Color.white;
+        AdvancedText.color = Color.white;
+        ExpertText.color = Color.white;
+    }
+
+    public void OnBeginnerButtonClicked()
+>>>>>>> Stashed changes
+    {
         GameManager.instance.LoadPlayMap(PlayMap.Easy);
     }
+<<<<<<< Updated upstream
+=======
+
+    public void OnBeginnerButtonPointerEnter()
+    {
+        BeginnerText.color = new Color32(130, 163, 140, 255);
+    }
+>>>>>>> Stashed changes
 
     public void OnHardButtonClicked()
     {
-        // 하드 모드를 선택하여 GameManager에 반영
         GameManager.instance.LoadPlayMap(PlayMap.Hard);
     }
+<<<<<<< Updated upstream
+=======
+    public void OnIntermediateButtonPointerEnter()
+    {
+        IntermediateText.color = new Color32(255, 226, 165, 255);
+    }
+>>>>>>> Stashed changes
 
     public void OnDesertButtonClicked()
     {
         GameManager.instance.LoadPlayMap(PlayMap.Desert);
     }
+<<<<<<< Updated upstream
+=======
+    public void OnAdvancedButtonPointerEnter()
+    {
+        AdvancedText.color = new Color32(239, 180, 255, 255);
+    }
+>>>>>>> Stashed changes
 
     public void OnDungeonButtonClicked()
     {
@@ -30,11 +75,15 @@ public class MainUI : MonoBehaviour
 
     public void OnExitButtonClicked()
     {
+<<<<<<< Updated upstream
     #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
     #else
         Application.Quit();
     #endif
+=======
+        ExpertText.color = new Color32(136, 255, 205, 255);
+>>>>>>> Stashed changes
     }
 
 }
