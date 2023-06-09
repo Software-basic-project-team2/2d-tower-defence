1049745ac1938cbdb5c952cf633b74c94d6560d0
lja3723 <lja3723@gmail.com>
2023-06-09 13:38:16 +0900

    Revert "Merge branch 'main' into 홍식"
    This reverts commit 483d0bb53e8f5b47f558bb7d38bc991c4c73da89.

--- Assets/Scripts/Manager/MainUI.cs
+++ Assets/Scripts/Manager/MainUI.cs
@@ -5,89 +5,65 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //MainScene의 이벤트 클래스
-public class MainUI : MonoBehaviour 
+public class MainUI : MonoBehaviour
 {
     public TMP_Text BeginnerText;
     public TMP_Text IntermediateText;
     public TMP_Text AdvancedText;
-    public TMP_Text ExperText;
-    private string PointerEnterText = "Play";
-    public Color c = new Color32(255, 0, 0, 255);
+    public TMP_Text ExpertText;
 
-    private void Start()
-    {
-        //BeginnerText.color = new Color32(255, 0, 0, 255);
-    }
-
-    private void ClearText()
-    {
-        BeginnerText.text = "";
-        IntermediateText.text = "";
-        AdvancedText.text = "";
-        ExperText.text = "";
-    }
-    private void ResetText()
-    {
-        BeginnerText.text = "Beginner";
-        BeginnerText.color = Color.white;
-        IntermediateText.text = "Intermediate";
-        IntermediateText.color = Color.white;
-        AdvancedText.text = "Advanced";
-        AdvancedText.color = Color.white;
-        ExperText.text = "Expert";
-        ExperText.color = Color.white;
-    }
     public void OnButtonPointerExit()
     {
-        ResetText();
+        BeginnerText.color = Color.white;
+        IntermediateText.color = Color.white;
+        AdvancedText.color = Color.white;
+        ExpertText.color = Color.white;
     }
 
     public void OnBeginnerButtonClicked()
     {
-        // 이지 모드를 선택하여 GameManager에 반영
         GameManager.instance.LoadPlayMap(PlayMap.Easy);
     }
+
+
     public void OnBeginnerButtonPointerEnter()
     {
-        ClearText();
-        BeginnerText.text = PointerEnterText;
         BeginnerText.color = new Color32(130, 163, 140, 255);
     }
 
-
-    public void OnIntermediateButtonClicked()
+    public void OnHardButtonClicked()
     {
-        // 하드 모드를 선택하여 GameManager에 반영
         GameManager.instance.LoadPlayMap(PlayMap.Hard);
     }
+
     public void OnIntermediateButtonPointerEnter()
     {
-        ClearText();
-        IntermediateText.text = PointerEnterText;
         IntermediateText.color = new Color32(255, 226, 165, 255);
     }
 
-
-    public void OnAdvancedButtonClicked()
+    public void OnDesertButtonClicked()
     {
         GameManager.instance.LoadPlayMap(PlayMap.Desert);
     }
+
     public void OnAdvancedButtonPointerEnter()
     {
-        ClearText();
-        AdvancedText.text = PointerEnterText;
         AdvancedText.color = new Color32(239, 180, 255, 255);
     }
 
-
-    public void OnExpertButtonClicked()
+    public void OnDungeonButtonClicked()
     {
         GameManager.instance.LoadPlayMap(PlayMap.Dungeon);
     }
-    public void OnExpertButtonPointerEnter()
+
+    public void OnExitButtonClicked()
     {
-        ClearText();
-        ExperText.text = PointerEnterText;
-        ExperText.color = new Color32(136, 255, 205, 255);
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+        ExpertText.color = new Color32(136, 255, 205, 255);
     }
+
 }
