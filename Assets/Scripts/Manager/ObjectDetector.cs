5b1d9b75c92c5f981ea88968a4eaf92b8ff069e5
ArkKorea <cjjj1102@naver.com>
2023-06-09 14:23:33 +0800

    타워 프리팹 수정 및 코드
    =

---
+++ Assets/Scripts/Manager/ObjectDetector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetector : MonoBehaviour
+{
+    [SerializeField]
+    private TowerDataViewer towerDataViewer;
+
+    private Camera mainCamera;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 마우스 좌클릭시
+        {
+            ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.CompareTag("Tile")) //바닥을 누르면 정보창 비활성화
+                {
+                   towerDataViewer.OffPanel();
+                }
+                else if (hit.transform.CompareTag("Tower")) // 타워를 누르면 정보창 활성화
+                {
+                   towerDataViewer.OnPanel();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Tower/TowerDataViewer.cs
+++ Assets/Scripts/Tower/TowerDataViewer.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerData : MonoBehaviour
+public class TowerDataViewer : MonoBehaviour
 {
+    private TowerDataViewer towerDataViewer;
+
     private Camera mainCamera;
     private Ray ray;
     private RaycastHit hit;
+
     private void Awake()
     {
-        OffPanel();
+       
+        OnPanel();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            OffPanel();
-        }
+        // if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    OffPanel();
+        // }    
+        //else if (Input.GetKeyDown(KeyCode.LeftControl))
+        //{
+        //    OnPanel();
+        //}
         if (Input.GetMouseButtonDown(0)) // 마우스 좌클릭시
         {
             ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity)) 
-            { 
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
                 if (hit.transform.CompareTag("Tile")) //바닥을 누르면 정보창 비활성화
                 {
-                    OffPanel();
+                    towerDataViewer.OffPanel();
                 }
                 else if (hit.transform.CompareTag("Tower")) // 타워를 누르면 정보창 활성화
                 {
-                    OnPanel();
+                    towerDataViewer.OnPanel();
                 }
             }
         }
