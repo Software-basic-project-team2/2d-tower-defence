54ff3f07aba0011d72126579d0c4e88ab4798838
테크 <lja3723@gmail.com>
2023-06-10 22:33:54 +0900

    파일통합

--- Assets/Scripts/UI/TowerInspectorUI.cs
+++ Assets/Scripts/UI/TowerInspectorUI.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class TowerInspectorUI : MonoBehaviour
 {
     [SerializeField]
-    private TowerDataViewer towerDataViewer;
+    private TextMeshProUGUI textDamage;
+    [SerializeField]
+    private TextMeshProUGUI textLevel;
+    [SerializeField]
+    private GameObject TowerInspector;
 
-    private Tower tower;
+    private Tower currentTower;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OffPanel();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (GameManager.instance.isPaused) return; //일시정지 상태인 경우 리턴
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OffPanel();
+        }
 
         if (Input.GetMouseButtonDown(0)) // 마우스 좌클릭시
         {
@@ -28,16 +38,41 @@ public class TowerInspectorUI : MonoBehaviour
 
             if (hit.transform.CompareTag("Tower")) // 타워를 누르면 정보창 활성화
             {
-                towerDataViewer.OnPanel(hit.transform);                    
+                OnPanel(hit.transform);                    
             }
             else if (hit.transform.CompareTag("s")) 
             {
-                towerDataViewer.OffPanel(); //바닥을 누르면 정보창 비활성화
+                OffPanel(); //바닥을 누르면 정보창 비활성화
             }
-        }
-        
+        }        
     }
 
-    
+    public bool isActive()
+    {
+        return TowerInspector.activeSelf;
+    }
+
+    public void OnPanel(Transform tower)
+    {
+        currentTower = tower.GetComponent<Tower>();
+        TowerInspector.SetActive(true);
+        UpdateTowerData();
+    }
+
+    public void OffPanel()
+    {
+        TowerInspector.SetActive(false);
+    }
+
+    private void UpdateTowerData()
+    {
+        textDamage.text = "Damage : " + currentTower.Damage;
+        textLevel.text = "Level : " + currentTower.Level;
+    }
+
+    public void OnUpgradeButtonClicked()
+    {
+        currentTower.LevelUp();
+    }
 
 }
